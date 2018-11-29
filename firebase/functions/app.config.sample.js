4ee51a085282e8e53d9523f209704a64c405db07
Syamsul Muttaqin <lombok.oc@gmail.com>
2018-11-29 21:31:50 +0700

    fixing some bugs on firebase functions

---
+++ firebase/functions/app.config.sample.js
@@ -0,0 +1,11 @@
+// rename this file to config.js
+exports.firebaseConfig = {
+  apiKey: "<GOOGLE API KEY>",
+  authDomain: "<FIREBASE AUTH DOMAIN>",
+  databaseURL: "<FIREBASE DATABASE URL>",
+  projectId: "<FIREBASE PROJECT ID>",
+  storageBucket: "<FIREBASE STORAGE BUCKET>",
+  messagingSenderId: "<SENDER ID>"
+}
+
+exports.QRValidationURL = '<QR Validation URL>';
